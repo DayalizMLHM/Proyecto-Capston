eb7fdf32ca1aa8c36d0c4f0324faadf671873ab6
LuisSanHer <luish_991@hotmail.com>
2022-02-17 01:49:19 +0000

    Se añadió el envio de msg por mqtt

--- somnolencia.py
+++ somnolencia.py
@@ -1,145 +1,153 @@
-# import the necessary packages
-from scipy.spatial import distance as dist
-from imutils.video import VideoStream
+'''This script detects if a person is drowsy or not,using dlib and eye aspect ratio
+calculations. Uses webcam video feed as input.'''
+
+#Import necessary libraries
+from paho.mqtt import client as mqtt_client
+from scipy.spatial import distance
 from imutils import face_utils
-from threading import Thread
 import numpy as np
-import playsound
-import argparse
-import imutils
+import pygame #For playing sound
 import time
 import dlib
 import cv2
-def sound_alarm(path):
-	# play an alarm sound
-	playsound.playsound(path)
-def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
+import random
 
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
-	C = dist.euclidean(eye[0], eye[3])
 
-	# compute the eye aspect ratio
-	ear = (A + B) / (2.0 * C)
+#Variables para publicar en MQTT
+broker = 'localhost'
+port = 1883
+topic = "python/mqtt"
 
-	# return the eye aspect ratio
-	return ear
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-ap.add_argument("-a", "--alarm", type=str, default="",
-	help="path alarm .WAV file")
-ap.add_argument("-w", "--webcam", type=int, default=0,
-	help="index of webcam on system")
-args = vars(ap.parse_args())
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold for to set off the
-# alarm
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 48
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
 
-# initialize the frame counter as well as a boolean used to
-# indicate if the alarm is going off
+#Initialize Pygame and load music
+pygame.mixer.init()
+pygame.mixer.music.load('audio/alarma.mp3')
+
+#Minimum threshold of eye aspect ratio below which alarm is triggerd
+EYE_ASPECT_RATIO_THRESHOLD = 0.3
+
+#Minimum consecutive frames for which eye ratio is below threshold for alarm to be triggered
+EYE_ASPECT_RATIO_CONSEC_FRAMES = 50
+
+#COunts no. of consecutuve frames below threshold value
 COUNTER = 0
-ALARM_ON = False
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
+
+#Load face cascade which will be used to draw a rectangle around detected faces.
+#face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+#Función para conectar al broker MQTT
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+#Función para publicar en el broker MQTT, se utiliza un topic conocido para poder leerlo por fuera
+def publish(client):
+    msg = f"Conductor dormido"
+    result = client.publish(topic, msg)
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+def enviar_mensaje_central():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    client.loop_stop(True)
+
+
+#This function calculates and return eye aspect ratio
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+
+    ear = (A+B) / (2*C)
+    return ear
+
+#Load face detector and predictor, uses dlib shape predictor file
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-vs = VideoStream(src=args["webcam"]).start()
-time.sleep(1.0)
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-# loop over frames from the video stream
-while True:
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale
-	# channels)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#Extract indexes of facial landmarks for the left and right eye
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
 
-	# detect faces in the grayscale frame
-	rects = detector(gray, 0)
-	# loop over the face detections
-	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a NumPy
-		# array
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
+#Start webcam video capture
+video_capture = cv2.VideoCapture(0)
 
-		# extract the left and right eye coordinates, then use the
-		# coordinates to compute the eye aspect ratio for both eyes
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
+#Give some time for camera to initialize(not required)
+time.sleep(2)
 
-		# average the eye aspect ratio together for both eyes
-		ear = (leftEAR + rightEAR) / 2.0
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-		# check to see if the eye aspect ratio is below the blink
-		# threshold, and if so, increment the blink frame counter
-		if ear < EYE_AR_THRESH:
-			COUNTER += 1
+while(True):
+    #Read each frame and flip it, and convert to grayscale
+    ret, frame = video_capture.read()
+    frame = cv2.flip(frame,1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-			# if the eyes were closed for a sufficient number of
-			# then sound the alarm
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
-				# if the alarm is not on, turn it on
-				if not ALARM_ON:
-					ALARM_ON = True
+    #Detect facial points through detector function
+    faces = detector(gray, 0)
 
-					# check to see if an alarm file was supplied,
-					# and if so, start a thread to have the alarm
-					# sound played in the background
-					if args["alarm"] != "":
-						t = Thread(target=sound_alarm,
-							args=(args["alarm"],))
-						t.deamon = True
-						t.start()
+    #Detect faces through haarcascade_frontalface_default.xml
+    face_rectangle = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-				# draw an alarm on the frame
-				cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    #Draw rectangle around each face detected
+    for (x,y,w,h) in face_rectangle:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-		# otherwise, the eye aspect ratio is not below the blink
-		# threshold, so reset the counter and alarm
-		else:
-			COUNTER = 0
-			ALARM_ON = False
-			# draw the computed eye aspect ratio on the frame to help
-		# with debugging and setting the correct eye aspect ratio
-		# thresholds and frame counters
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
- 
-	# show the frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    #Detect facial points
+    for face in faces:
 
-# do a bit of cleanup
+        shape = predictor(gray, face)
+        shape = face_utils.shape_to_np(shape)
+
+        #Get array of coordinates of leftEye and rightEye
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+
+        #Calculate aspect ratio of both eyes
+        leftEyeAspectRatio = eye_aspect_ratio(leftEye)
+        rightEyeAspectRatio = eye_aspect_ratio(rightEye)
+
+        eyeAspectRatio = (leftEyeAspectRatio + rightEyeAspectRatio) / 2
+
+        #Use hull to remove convex contour discrepencies and draw eye shape around eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        #Detect if eye aspect ratio is less than threshold
+        if(eyeAspectRatio < EYE_ASPECT_RATIO_THRESHOLD):
+            COUNTER += 1
+            #If no. of frames is greater than threshold frames,
+            if COUNTER >= EYE_ASPECT_RATIO_CONSEC_FRAMES:
+                pygame.mixer.music.play(-1)
+                enviar_mensaje_central();
+                cv2.putText(frame, "You are Drowsy", (150,200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)
+        else:
+            pygame.mixer.music.stop()
+            COUNTER = 0
+
+    #Show video feed
+    cv2.imshow('Video', frame)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+#Finally when video capture is over, release the video capture and destroyAllWindows
+video_capture.release()
 cv2.destroyAllWindows()
-vs.stop()
+
+
