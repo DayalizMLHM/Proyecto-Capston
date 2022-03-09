b6f71ec3cac7ac96ad0ac078528f85b1078fcef1
LuisSanHer <luish_991@hotmail.com>
2022-03-09 00:14:46 +0000

    Cambios minimos

--- somnolencia.py
+++ somnolencia.py
@@ -1,14 +1,10 @@
-'''This script detects if a person is drowsy or not,using dlib and eye aspect ratio
-calculations. Uses webcam video feed as input.'''
-
-
-#Import necessary libraries
+#Librerias necesarias
 from paho.mqtt import client as mqtt_client
 from scipy.spatial import distance
 from imutils import face_utils
 from datetime import datetime
 import numpy as np
-import pygame #For playing sound
+import pygame #Para hacer sonar la alarma
 import time
 import dlib
 import cv2
@@ -21,24 +17,24 @@ broker = 'broker.hivemq.com'
 port = 1883
 topic = "somnolencia/luisangel"
 estado = 0 # 0 Despierto - 1 Dormido
-# generate client ID with pub prefix randomly
+#Generar cliente para mqtt
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 
-#Initialize Pygame and load music
+#Inicializar pygame y reproductor de audio (cargar)
 pygame.mixer.init()
 pygame.mixer.music.load('audio/alarma.mp3')
 
-#Minimum threshold of eye aspect ratio below which alarm is triggerd
+#Umbral mínimo de la relación de aspecto de los ojos por debajo del cual se activa la alarma
 EYE_ASPECT_RATIO_THRESHOLD = 0.3
 
-#Minimum consecutive frames for which eye ratio is below threshold for alarm to be triggered
+#Fotogramas consecutivos mínimos en los que la proporción de ojos está por debajo del umbral para que se active la alarma
 EYE_ASPECT_RATIO_CONSEC_FRAMES = 50
 
-#COunts no. of consecutuve frames below threshold value
+#Cuenta No. de fotogramas consecutivos por debajo del valor umbral
 COUNTER = 0
 
-#Load face cascade which will be used to draw a rectangle around detected faces.
-#face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+#Cargue la cascada de caras que se usará para dibujar un rectángulo alrededor de las caras detectadas.
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 #Función para conectar al broker MQTT
@@ -78,7 +74,7 @@ def enviar_mensaje_central(topic, counter):
     client.loop_stop(True)
 
 
-#This function calculates and return eye aspect ratio
+#Esta función calcula y devuelve la relación de aspecto de los ojos
 def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
@@ -87,64 +83,64 @@ def eye_aspect_ratio(eye):
     ear = (A+B) / (2*C)
     return ear
 
-#Load face detector and predictor, uses dlib shape predictor file
+#Cargue el detector y el predictor de rostros, utiliza el archivo predictor de forma dlib
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('../shape_predictor_68_face_landmarks.dat')
 
-#Extract indexes of facial landmarks for the left and right eye
+#Extraiga índices de puntos de referencia faciales para el ojo izquierdo y derecho
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
 
-#Start webcam video capture
+#Inicie la captura de video de la cámara web
 video_capture = cv2.VideoCapture(0)
 
-#Give some time for camera to initialize(not required)
+#Dé algo de tiempo para que la cámara se inicialice (no es necesario)
 time.sleep(2)
 
-#os.system("lxterminal -e 'python3 distancia.py'")
+os.system("lxterminal -e 'python3 distancia.py'")
 
 while(True):
-    #Read each frame and flip it, and convert to grayscale
+    #Lea cada cuadro y gírelo, y conviértalo a escala de grises
     ret, frame = video_capture.read()
     frame = cv2.flip(frame,1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #Detect facial points through detector function
+    #Detecta puntos faciales a través de la función de detector.
     faces = detector(gray, 0)
 
-    #Detect faces through haarcascade_frontalface_default.xml
+    #Detectar rostros a través de haarcascade_frontalface_default.xml
     face_rectangle = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    #Draw rectangle around each face detected
+    #Dibuja un rectángulo alrededor de cada cara detectada
     for (x,y,w,h) in face_rectangle:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-    #Detect facial points
+    #Detecta puntos faciales
     for face in faces:
 
         shape = predictor(gray, face)
         shape = face_utils.shape_to_np(shape)
 
-        #Get array of coordinates of leftEye and rightEye
+        #Obtener las coordenadas del ojo izquierdo y derecho
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
 
-        #Calculate aspect ratio of both eyes
+        #Calcula aspecto de ojos
         leftEyeAspectRatio = eye_aspect_ratio(leftEye)
         rightEyeAspectRatio = eye_aspect_ratio(rightEye)
 
         eyeAspectRatio = (leftEyeAspectRatio + rightEyeAspectRatio) / 2
 
-        #Use hull to remove convex contour discrepencies and draw eye shape around eyes
+        #Use el casco para eliminar las discrepancias de contorno convexo y dibuje la forma de los ojos alrededor de los ojos
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-        #Detect if eye aspect ratio is less than threshold
+        #Detectar si la relación de aspecto de los ojos es inferior al umbral
         if(eyeAspectRatio < EYE_ASPECT_RATIO_THRESHOLD):
             COUNTER += 1
-            #If no. of frames is greater than threshold frames,
+            #Si el No. de fotogramas es mayor que el umbral de fotogramas
             if COUNTER >= EYE_ASPECT_RATIO_CONSEC_FRAMES:
                 estado = 1
                 enviar_mensaje_central(topic, estado);
@@ -157,12 +153,12 @@ while(True):
                 estado = 0
                 enviar_mensaje_central(topic, estado);
 
-    #Show video feed
+    #Mostrar video
     cv2.imshow('Video', frame)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
-#Finally when video capture is over, release the video capture and destroyAllWindows
+#Finalmente, cuando termine la captura de video, suelte la captura de video y destruya Todas las ventanas
 video_capture.release()
 cv2.destroyAllWindows()
 
